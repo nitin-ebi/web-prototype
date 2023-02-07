b9add7449eb3d76ed04c95ad73e16daec50e356c
nkumar2 <nkumar2@ebi.ac.uk>
2023-02-07 02:45:59 +0000

    GenotypesFilter component test

--- src/__tests__/GenotypesFilter.test.js
+++ src/__tests__/GenotypesFilter.test.js
@@ -1,129 +1,47 @@
 import React from 'react'
-
 import {render, screen} from '@testing-library/react'
-
 import GenotypesFilter from "../components/GenotypesFilter"
+import {genotype_filter_test_data} from "../test_data/GenotypesFilterData"
+import '@testing-library/jest-dom/extend-expect'
 
 test('check genotype filter view component is loading everything correctly', () => {
-    const test_props = {
-        "speciesList": [
-            {
-                "taxonomyCode": "ecaballus",
-                "taxonomyCommonName": "Horse",
-                "taxonomyScientificName": "Equus caballus"
-            },
-        ],
-        "speciesAssemblyMap": {
-            "ecaballus": [
-                {
-                    "assemblyAccession": "GCA_000002305.1",
-                    "assemblyChain": "GCA_000002305",
-                    "assemblyVersion": "1",
-                    "assemblyName": "EquCab2.0",
-                    "assemblyCode": "20",
-                    "taxonomyId": 9796,
-                    "taxonomyCommonName": "Horse",
-                    "taxonomyScientificName": "Equus caballus",
-                    "taxonomyCode": "ecaballus",
-                    "taxonomyEvaName": "horse",
-                    "speciesAssemblyCode": "ecaballus_20"
-                },
-                {
-                    "assemblyAccession": "GCA_002863925.1",
-                    "assemblyChain": "GCA_002863925",
-                    "assemblyVersion": "1",
-                    "assemblyName": "EquCab3.0",
-                    "assemblyCode": "30",
-                    "taxonomyId": 9796,
-                    "taxonomyCommonName": "Horse",
-                    "taxonomyScientificName": "Equus caballus",
-                    "taxonomyCode": "ecaballus",
-                    "taxonomyEvaName": "horse",
-                    "speciesAssemblyCode": "ecaballus_30"
-                }
-            ]
-        },
-        "assemblyList": [
-            {
-                "assemblyAccession": "GCA_000002305.1",
-                "assemblyChain": "GCA_000002305",
-                "assemblyVersion": "1",
-                "assemblyName": "EquCab2.0",
-                "assemblyCode": "20",
-                "taxonomyId": 9796,
-                "taxonomyCommonName": "Horse",
-                "taxonomyScientificName": "Equus caballus",
-                "taxonomyCode": "ecaballus",
-                "taxonomyEvaName": "horse",
-                "speciesAssemblyCode": "ecaballus_20"
-            },
-            {
-                "assemblyAccession": "GCA_002863925.1",
-                "assemblyChain": "GCA_002863925",
-                "assemblyVersion": "1",
-                "assemblyName": "EquCab3.0",
-                "assemblyCode": "30",
-                "taxonomyId": 9796,
-                "taxonomyCommonName": "Horse",
-                "taxonomyScientificName": "Equus caballus",
-                "taxonomyCode": "ecaballus",
-                "taxonomyEvaName": "horse",
-                "speciesAssemblyCode": "ecaballus_30"
-            }
-        ],
-        "selectedSpecies": "ecaballus",
-        "selectedAssembly": "ecaballus_20",
-        "location": "1:3000000-3100000",
-        "studies": [
-            "BROAD_EQUCAB2.0:2008.08.08",
-            "PRJEB22866",
-            "PRJEB23301",
-            "PRJEB23441",
-            "PRJEB24624",
-            "PRJEB24630",
-            "PRJEB25439",
-            "PRJEB26741",
-            "PRJEB27771",
-            "PRJEB30116",
-            "PRJEB32686",
-            "PRJEB38010",
-            "PRJEB9799"
-        ],
-        "selectedStudies": [
-            "BROAD_EQUCAB2.0:2008.08.08",
-            "PRJEB22866",
-            "PRJEB23301",
-            "PRJEB23441",
-            "PRJEB24624",
-            "PRJEB24630",
-            "PRJEB25439",
-            "PRJEB26741",
-            "PRJEB27771",
-            "PRJEB30116",
-            "PRJEB32686",
-            "PRJEB38010",
-            "PRJEB9799"
-        ],
-        "resultsPerPage": 50
-    };
 
     render(<GenotypesFilter
-        speciesList={test_props.speciesList}
-        speciesAssemblyMap={test_props.speciesAssemblyMap}
-        assemblyList={test_props.assemblyList}
-        selectedSpecies={test_props.selectedSpecies}
-        selectedAssembly={test_props.selectedAssembly}
-        location={test_props.location}
-        studies={test_props.studies}
-        selectedStudies = {test_props.selectedStudies}
-        resultsPerPage={test_props.resultsPerPage}
+        speciesList={genotype_filter_test_data.speciesList}
+        speciesAssemblyMap={genotype_filter_test_data.speciesAssemblyMap}
+        assemblyList={genotype_filter_test_data.assemblyList}
+        selectedSpecies={genotype_filter_test_data.selectedSpecies}
+        selectedAssembly={genotype_filter_test_data.selectedAssembly}
+        location={genotype_filter_test_data.location}
+        studies={genotype_filter_test_data.studies}
+        selectedStudies = {genotype_filter_test_data.selectedStudies}
+        resultsPerPage={genotype_filter_test_data.resultsPerPage}
         />)
 
+    // assert species drop down
     expect(screen.getByRole('option', { name: 'Equus caballus' }).selected).toBe(true)
+    expect(screen.getByRole('option', { name: 'Gallus gallus' }).selected).toBe(false)
+    // assert assembly drop down
     expect(screen.getByRole('option', { name: 'EquCab2.0' }).selected).toBe(true)
     expect(screen.getByRole('option', { name: 'EquCab3.0' }).selected).toBe(false)
-    expect(screen.getByRole('option', { name: '10' }).selected).toBe(false)
+    //assert location textbox
+    expect(screen.getByRole('textbox').value).toBe('1:3000000-3100000')
+    //assert results per page drop down
     expect(screen.getByRole('option', { name: '50' }).selected).toBe(true)
+    expect(screen.getByRole('option', { name: '10' }).selected).toBe(false)
     expect(screen.getByRole('option', { name: '100' }).selected).toBe(false)
-
+    //assert studies checkboxes
+    expect(screen.getByRole('checkbox', {name: 'BROAD_EQUCAB2.0:2008.08.08'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB22866'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB23301'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB23441'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB24624'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB24630'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB25439'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB26741'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB27771'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB30116'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB32686'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB38010'})).toBeChecked()
+    expect(screen.getByRole('checkbox', {name: 'PRJEB9799'})).toBeChecked()
 })
