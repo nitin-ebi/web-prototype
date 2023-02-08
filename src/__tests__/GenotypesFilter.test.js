e111aee1302600a3a63bac8ec1c4d75ea94a1a8d
nkumar2 <nkumar2@ebi.ac.uk>
2023-02-08 12:51:29 +0000

    Genotypes Table Data test

--- src/__tests__/GenotypesFilter.test.js
+++ src/__tests__/GenotypesFilter.test.js
@@ -1,36 +1,44 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
-import GenotypesFilter from "../components/GenotypesFilter"
-import {genotype_filter_test_data} from "../test_data/GenotypesFilterData"
 import '@testing-library/jest-dom/extend-expect'
+import GenotypesFilter from "../components/GenotypesFilter"
+import {genotypes_filter_test_data} from "../__tests_data/GenotypesFilterData"
+
 
 test('check genotype filter view component is loading everything correctly', () => {
 
     render(<GenotypesFilter
-        speciesList={genotype_filter_test_data.speciesList}
-        speciesAssemblyMap={genotype_filter_test_data.speciesAssemblyMap}
-        assemblyList={genotype_filter_test_data.assemblyList}
-        selectedSpecies={genotype_filter_test_data.selectedSpecies}
-        selectedAssembly={genotype_filter_test_data.selectedAssembly}
-        location={genotype_filter_test_data.location}
-        studies={genotype_filter_test_data.studies}
-        selectedStudies = {genotype_filter_test_data.selectedStudies}
-        resultsPerPage={genotype_filter_test_data.resultsPerPage}
+        speciesList={genotypes_filter_test_data.speciesList}
+        speciesAssemblyMap={genotypes_filter_test_data.speciesAssemblyMap}
+        assemblyList={genotypes_filter_test_data.assemblyList}
+        selectedSpecies={genotypes_filter_test_data.selectedSpecies}
+        selectedAssembly={genotypes_filter_test_data.selectedAssembly}
+        location={genotypes_filter_test_data.location}
+        studies={genotypes_filter_test_data.studies}
+        selectedStudies = {genotypes_filter_test_data.selectedStudies}
+        resultsPerPage={genotypes_filter_test_data.resultsPerPage}
         />)
 
-    // assert species drop down
+    // assert species drop down (Equus caballus) is the one selected
     expect(screen.getByRole('option', { name: 'Equus caballus' }).selected).toBe(true)
+    // assert species drop down (Gallus gallus) is there in the options but not selected
     expect(screen.getByRole('option', { name: 'Gallus gallus' }).selected).toBe(false)
-    // assert assembly drop down
+
+    // assert assembly drop down (EquCab2.0) is the one selected
     expect(screen.getByRole('option', { name: 'EquCab2.0' }).selected).toBe(true)
+    // assert assembly drop down (EquCab3.0) is there in the options but not selected
     expect(screen.getByRole('option', { name: 'EquCab3.0' }).selected).toBe(false)
-    //assert location textbox
+
+    // assert location textbox
     expect(screen.getByRole('textbox').value).toBe('1:3000000-3100000')
-    //assert results per page drop down
+
+    // assert results per page drop down - 50 is selected
     expect(screen.getByRole('option', { name: '50' }).selected).toBe(true)
+    // assert results per page drop down - 10 and 100 are present in options but not selected
     expect(screen.getByRole('option', { name: '10' }).selected).toBe(false)
     expect(screen.getByRole('option', { name: '100' }).selected).toBe(false)
-    //assert studies checkboxes
+
+    // assert studies checkboxes are present and checked
     expect(screen.getByRole('checkbox', {name: 'BROAD_EQUCAB2.0:2008.08.08'})).toBeChecked()
     expect(screen.getByRole('checkbox', {name: 'PRJEB22866'})).toBeChecked()
     expect(screen.getByRole('checkbox', {name: 'PRJEB23301'})).toBeChecked()
---
+++ src/__tests__/GenotypesTable.test.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import {genotypes_table_test_data} from "../__tests_data/GenotypesTableData"
+import GenotypesTable from "../components/GenotypesTable"
+
+test('check genotype table view component is loading everything correctly', () => {
+
+    render(<GenotypesTable
+        studies={genotypes_table_test_data.studies}
+        variants={genotypes_table_test_data.variants}
+        isLoaded={genotypes_table_test_data.isLoaded}
+        currentPage={genotypes_table_test_data.currentPage}
+        numPages={genotypes_table_test_data.numPages}
+    />)
+
+    // assert no of rows
+    const rows = screen.getAllByRole('row')
+    expect(rows).toHaveLength(51)
+    // assert no of columns
+    const cells = rows[0].cells;
+    expect(cells).toHaveLength(17)
+
+    // assert table header row
+    expect(rows[0].cells[0].textContent).toBe('Chr')
+    expect(rows[0].cells[1].textContent).toBe('Pos')
+    expect(rows[0].cells[2].textContent).toBe('Ref')
+    expect(rows[0].cells[3].textContent).toBe('Alt')
+    for (let index = 4; index < cells.length; index++) {
+        expect(rows[0].cells[index].textContent).toBe(genotypes_table_test_data.studies[index - 4])
+    }
+
+    //assert variants data
+    for (let row_id = 1; row_id < rows.length; row_id++) {
+        let json_variant = genotypes_table_test_data.variants[row_id - 1]
+        expect(rows[row_id].cells[0].textContent).toBe(json_variant.chromosome)
+        expect(rows[row_id].cells[1].textContent).toBe("" + json_variant.start)
+        expect(rows[row_id].cells[2].textContent).toBe(json_variant.reference)
+        expect(rows[row_id].cells[3].textContent).toBe(json_variant.alternate)
+
+        expect(rows[row_id].cells[3].id).toBe(json_variant.chromosome + "_" + json_variant.start + "_" + json_variant.reference + "_" + json_variant.alternate)
+        expect(rows[row_id].cells[3].title).toBe(json_variant.consequenceName)
+        expect(rgbToHex(rows[row_id].cells[3].style.backgroundColor)).toBe(genotypes_table_test_data.variants[row_id-1].color)
+
+        for (let cell_id = 4; cell_id < cells.length; cell_id++) {
+            let study_id = rows[0].cells[cell_id].textContent
+            let json_value = json_variant.alleleFreqs[study_id]
+            expect(rows[row_id].cells[cell_id].textContent).toBe(json_value == undefined ? "" : "" + json_value)
+        }
+    }
+
+})
+
+
+function rgbToHex(rgb) {
+    let rgb_val = rgb.substring(rgb.indexOf('(')+1,rgb.lastIndexOf(')')).split(",")
+    let r = rgb_val[0]
+    let g = rgb_val[1]
+    let b = rgb_val[2]
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)
+}
