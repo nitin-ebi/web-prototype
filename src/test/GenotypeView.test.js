886e5a981e91c1510780a0e761a4a1b81d83906a
nkumar2 <nkumar2@ebi.ac.uk>
2023-02-10 09:14:55 +0000

    mock api in genotypesview component

--- src/test/GenotypeView.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import {render} from '@testing-library/react'
-
-import GenotypeView from "../GenotypeView"
-
-test('check genotype view component is loading everything correctly', () => {
-    render(<GenotypeView>{}</GenotypeView>)
-})
